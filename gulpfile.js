aa4fad9182259e96528ef55b0f307ab6b37f743e
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2023-08-18 23:32:55 -0600

    agregando dependencias entre ellas para minificar imagenes y obtener webp

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,9 @@ const autoprefixer = require('autoprefixer');
 // "browserslist": ["last 1 version", "> 1%" ] --- con esto le dices todos los buscadores 
 // que tienen segun "CAN I USE" más del 1% de uso y en su ultima version. 
 
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
 //----------------------------------------------------------------
 
 // compilar sass
@@ -23,18 +26,38 @@ function css(done) {
     done(); //para que termine la compilacion
 }
 
+function imagenes() {
+    return src('src/img/**/*') // --- * -Carga todos los archivos
+        .pipe( imagemin({ optimizationLevel: 3 }))
+        .pipe( dest('build/img'))  // En vez de usar done usamos return. Ambas son validas. 
+}
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp())
+        .pipe( dest('build/img')) //
+}
 
 // el watch es para que se repita funcion css sin requerir estar compilando todas las veces
 function dev (done) {
-    watch('src/scss/**/*.scss', css); // --- **-busca en la carpeta scss --- *-cualquier archivos que terminan scss.
+    watch('src/scss/**/*.scss', css); // -**-busca en la carpeta scss - *.scss -cualquier archivos que terminan scss.
     /* watch('src/scss/app.scss', css); */ // Para una ubicación en particular
+    watch('src/img/**/*', imagenes); // Se integren y se vayan a build las nuevas imagenes
 }
 
 //----------------------------------------------------------------
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev ); //la que tiene el watch hasta el final porque esa detiene la ejecución
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.default = series( imagenes, versionWebp, css, dev ); //la que tiene el watch hasta el final porque esa detiene la ejecución
 
 // series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente.
 // parallel - Todas inician al mismo tiempo
+
+//----------------------------------------------------------------
+// En los otros documentos (app.scss, y los que terminan en .scss):
+
+//      @forward - archivos dentro de otros pero solo los incluye, no los usa.
+//      @use ó @import - incluirlos y compilarlos. 
